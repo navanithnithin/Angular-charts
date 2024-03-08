ea2f278887ea545d3e0756f42e7010ad9a9df563
navanithnithin <navanithnithin@gmail.com>
2024-03-08 18:22:37 +0530

    my firat chart commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart
+} from "ng-apexcharts";
+import { ChartComponent } from "ng-apexcharts";
+
+export type ChartOptions1 = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-charts';
+  @ViewChild("chartObj") chart: ChartComponent;
+  public chartOptions1: Partial<ChartOptions1>;
+  constructor(){
+    this.chartOptions1 = {
+      series: [44, 55, 13, 43, 22],
+      chart: {
+        type: "donut"
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 1550
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+  }
 }
